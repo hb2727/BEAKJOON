7affb510feee06e1e8f6a4c0f1cd1fbdba678582
YHB <hb0854@naver.com>
2020-10-08 14:53:34 +0900

    10.8 commit

---
+++ 2stage/beak_9498.java
@@ -0,0 +1,30 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class beak_9498 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int score = scan.nextInt();
+		
+		scan.close();
+		
+		if(score >= 90) {
+			System.out.println("A");
+		}else if(score >= 80) {
+			System.out.println("B");
+		}else if(score >= 70) {
+			System.out.println("C");
+		}else if(score >= 60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+		
+		
+		
+	}
+}
