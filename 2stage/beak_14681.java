11a6d837123e39ce459f2dd284816587f78469b0
YHB <hb0854@naver.com>
2020-10-10 13:34:47 +0900

    10.10 commit

---
+++ 2stage/beak_14681.java
@@ -0,0 +1,30 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class beak_14681 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		
+		if(x>0) {
+			if(y>0) {
+				System.out.println("1");
+			}else {
+				System.out.println("4");
+			}
+		}else {
+			if(y>0) {
+				System.out.println("2");
+			}else {
+				System.out.println("3");
+			}
+		}
+		
+		
+		
+	}
+}
