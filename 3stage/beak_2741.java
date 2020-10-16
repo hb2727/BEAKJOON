518a226a14313a3008a69fad43e642df07c83cc1
YHB <hb0854@naver.com>
2020-10-16 18:50:59 +0900

    10.16 commit

---
+++ 3stage/beak_2741.java
@@ -0,0 +1,18 @@
+package beakjoon;
+
+import java.util.*;
+public class beak_2741 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		scan.close();
+		
+		for(int i=1; i<=n;i++) {
+			System.out.println(i);
+		}
+	}
+}
