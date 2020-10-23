31c589175dbeacb4792b6f1e8032accc58400ce7
YHB <hb0854@naver.com>
2020-10-23 16:52:38 +0900

    10.23 commit

---
+++ 4stage/beak_10952.java
@@ -0,0 +1,18 @@
+package beakjoon;
+import java.util.*;
+public class beak_10952 {
+
+	
+
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			
+			while(true) {
+				int a = scan.nextInt();
+				int b = scan.nextInt();
+				if(a==0&&b==0) break;
+				System.out.println(a+b);
+			}
+		}
+	}
+
