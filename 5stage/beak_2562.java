05e0282424a29d7c345b2877ca43c8d50366de36
YHB <hb0854@naver.com>
2020-10-28 22:18:14 +0900

    10.28 commit

---
+++ 5stage/beak_2562.java
@@ -0,0 +1,22 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class beak_2562 {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int num = 0;
+    	int i = 1;
+    	int place = 0;
+    	while (i <= 9) {
+    		int newNum = sc.nextInt();
+    		if (newNum > num) {
+    			num = newNum;
+    			place = i;
+    		}
+                		i++;
+        }
+    	System.out.println(num);
+    	System.out.print(place);
+    }
+}
